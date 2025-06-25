6ccc30041f0f98f1022fecc13863a930e2532217
Chetan Nagoriya <152314423+chetannagoriya@users.noreply.github.com>
2025-06-25 15:17:47 +0530

    Create swaping.py

---
+++ swaping.py
@@ -0,0 +1,11 @@
+# swaping two number using python
+x=input("Enter value of x :")
+y=input("Enter value of y :")
+temp=y
+y=x
+print(f"vlaue of x is {temp} and y is {y}")
+
+'''output
+Enter value of x :1
+Enter value of y :2
+vlaue of x is 2 and y is 1'''
