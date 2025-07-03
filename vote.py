ca939342bc90315a401c7dd2274f10f16e5f4c8d
Chetan Nagoriya <152314423+chetannagoriya@users.noreply.github.com>
2025-07-03 17:13:03 +0530

    Create vote.py

---
+++ vote.py
@@ -0,0 +1,11 @@
+
+#write a program in python to check you are eligible for vote or not
+print("To check you are eligible for vote or not ! ")
+age=int(input("Enter the age:"))
+print(f"your age is {age}")
+
+
+if(age>=18):
+    print("you are eligible for vote")
+else:
+    print("you are not eligible for vote")
