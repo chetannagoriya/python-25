714304f1daa174398da6110aff92e6993a9c8aa2
Chetan Nagoriya <152314423+chetannagoriya@users.noreply.github.com>
2025-07-03 22:42:31 +0530

    Create grade.py

---
+++ grade.py
@@ -0,0 +1,14 @@
+
+# Program for assin grade form marks !
+marks=int(input("Enter the Marks:"))
+
+if(marks>=90):
+    print("A")
+elif(90>marks>=80):
+    print("B")
+elif(80>marks>=70):
+    print("C")
+else:
+    print("D")
+
+print("Program is End !")
